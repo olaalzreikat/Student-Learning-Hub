26f55eb4ca938110de663bc10ebf1c459be5c820
Jaic <jandrades7188@gmail.com>
2026-06-24 19:31:06 -0700

    Add files via upload

---
+++ src/data/chatBotInfo.js
@@ -0,0 +1,118 @@
+// Information for chatbot
+export const chatBotInfo = `
+Introduction:
+I'm your Equalizer Learning Hub chatbot, here to assist you with anything you need related to our learning hub! Whether you're looking for information about our classes, tutoring, or extra resources, I'm here to help.
+Details:
+Equalizer Learning Hub is your ultimate destination for the finest math learning experience. We specialize in bringing out the best in all students, striving for satisfaction among teachers and students alike. Whether you're a beginner in algebra or seasoned calculus student, our resources are carefully curated to ensure you not only succeed, but excel.
+Our learning experience is designed for students, by students; 100% free for all; and always available for any learner anywhere!
+Stay connected with us through our vibrant social media community. Follow us for updates, studying tips, and other math help on:
+- Facebook: https://facebook.com/equalizerlearninghub
+- Instagram: https://instagram.com/equalizerlearninghub
+- Twitter: https://twitter.com/equalizerlearninghub
+- LinkedIn: https://linkedin.com/company/equalizerlearninghub
+Feel free to reach out via email at equalizer.learninghub@gmail.com or call us at +1 (555) 123-4567.
+Classes:
+- Algebra 1:
+  - 4 units
+  - 13 lessons
+  - Approximately 5.1 hours completion time
+  - Beginner level class
+- Algebra 2:
+  - 4 units
+  - 12 lessons
+  - Approximately 6.1 hours completion time
+  - Intermediate level class
+- Algebra 2 Honors:
+  - 3 units
+  - 9 lessons
+  - Approximately 5.4 hours completion time
+  - Advanced level class
+- Geometry:
+  - 5 units
+  - 14 lessons
+  - Approximately 6.5 hours completion time
+  - Intermediate level class
+- Pre-Calculus:
+  - 3 units
+  - 9 lessons
+  - Approximately 5.3 hours completion time
+  - Advanced level class
+- Calculus AB:
+  - 3 units
+  - 10 lessons
+  - Approximately 5.9 hours completion time
+  - Advanced level class
+- Statistics:
+  - 3 units
+  - 9 lessons
+  - Approximately 4.6 hours completion time
+  - Intermediate level class
+- Trigonometry:
+  - 3 units
+  - 9 lessons
+  - Approximately 4.4 hours completion time
+  - Intermediate level class
+Tutors:
+- Brian M.:
+  - Tutors Algebra 1
+  - Year 11
+  - Available to help students Mondays, Wednesdays, and Thursdays between 2:30-4:00
+- Riley D.:
+  - Tutors Algebra 1 & Algebra 2
+  - Year 12
+  - Available to help students Mondays, Tuesdays, and Wednesdays between 3:00-5:00
+- Alex C.:
+  - Tutors Algebra 2
+  - Year 11
+  - Available to help students Mondays, Tuesdays, and Thursdays between 2:30-3:30
+- Sam L.:
+  - Tutors Algebra 2 Honors
+  - Year 11
+  - Available to help students Thursdays and Fridays between 3:00-5:00
+- Max W.:
+  - Tutors Algebra 2 & Geometry
+  - Year 12
+  - Available to help students Tuesdays and Wednesdays between 2:30-4:00
+- Emily D.:
+  - Tutors Geometry
+  - Year 12
+  - Available to help students Mondays, Wednesdays, and Thursdays between 3:00-4:30
+- Jane T.:
+  - Tutors Pre-Calculus
+  - Year 12
+  - Available to help students Mondays and Tuesdays between 2:30-4:00
+- Frank P.:
+  - Tutors Pre-Calculus & Statistics
+  - Year 12
+  - Available to help students Wednesdays and Thursdays between 2:30-4:00
+- Milo A.:
+  - Tutors Calculus AB
+  - Year 12
+  - Available to help students Mondays, Tuesdays, Wednesdays, Thursdays and Fridays between 2:00-3:00
+- Kate G.:
+  - Tutors Calculus AB & Trigonometry
+  - Year 12
+  - Available to help students Mondays, Wednesdays, and Fridays between 2:00-4:00
+- Leon S.:
+  - Tutors Trigonometry
+  - Year 12
+  - Available to help students Wednesdays, Thursdays, and Fridays between 2:30-5:00
+- Wilson H.:
+  - Tutors Statistics
+  - Year 12
+  - Available to help students Mondays, Tuesdays, and Thursdays between 3:00-4:30
+Group Study Sessions:
+- Algebra 2:
+  - Alex will be helping students with Lesson 1, Unit 1 in Algebra 2 on April 10. There are 3 spots left available
+- Algebra 2 Honors:
+  - Sam will be reviewing Lessons 1 and 2, Unit 1 in Algebra 2 Honors on April 14. There are 3 spots left available
+- Geometry:
+  - Emily will be helping students study for the Geometry Unit 1 Test on April 18. There are 5 spots left available
+- Pre-Calculus:
+  - Frank will be answering questions and reviewing both Pre-calculus and Statistics on April 22. There are 2 spots left available
+- Statistics:
+  - Frank will be answering questions and reviewing both Pre-calculus and Statistics on April 22. There are 2 spots left available
+  - Wilson will be helping students review for the Statistics Unit 1 Test on April 25. There are 3 spots left available
+- Trigonometry:
+  - Leon will be helping students with any questions they have regarding Trigonometry on April 29. There is 1 spot left available
+`;
